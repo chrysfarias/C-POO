ad63bf595768277b24ec6d15b507d51f9ca2f61f
chrystian farias <chrstianb4mv@gmail.com>
2022-01-26 10:58:32 -0400

    adicionando metodos a classe ContaCorrente

---
+++ ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    
+    public void Sacar(double valor) //verificar se o saldo é menor que o valor
+    {
+        if (this.saldo < valor)
+        {
+            Console.WriteLine("Operação nao realizada por falta de Saldo");
+        }
+    
+        this.saldo -= valor;
+                  
+    }
+
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino) 
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+        
+        
+    }
+
+}
---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+             ContaCorrente  contaBruno   = new ContaCorrente();  // instanciei o objeto
+             
+             ContaCorrente  contaGabriela = new ContaCorrente();
+
+            Console.WriteLine(contaBruno.saldo);
+
+            contaBruno.Transferir(50,contaGabriela);
+
+            Console.WriteLine(contaBruno.saldo);
+            Console.WriteLine(contaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
