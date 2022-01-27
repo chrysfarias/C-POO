5dc2cbfdab01e8ecf619cceeddc04fe2b3b92ddb
chrystian farias <chrstianb4mv@gmail.com>
2022-01-27 08:51:56 -0400

    adicionando uma classe teste Herdeiro

---
+++ ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using _05_ByteBank;
+
+namespace _05_ByteBank 
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+
+        public void Sacar(double valor) //verificar se o saldo é menor que o valor
+        {
+            if (this.saldo < valor)
+            {
+                Console.WriteLine("Operação nao realizada por falta de Saldo");
+            }
+
+            this.saldo -= valor;
+
+        }
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+
+        }
+
+    }   
+
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente(); // criando um objeto
+            ContaCorrente conta = new ContaCorrente();
+            Herdeiro herdeiro = new Herdeiro();
+
+            gabriela.nome = "Gabriela";
+            gabriela.cpf = "20092822";
+            gabriela.profissao = "Engenheira mecânica";
+
+            conta.titular = gabriela;
+
+            gabriela.filho = herdeiro;
+
+            
+
+            herdeiro.nome = "Antônio Nunes";
+            herdeiro.escolaridade = "Ensino médio";
+            herdeiro.idade = 17;
+            herdeiro.sexo = "Masculino";
+
+           
+
+
+            //teste
+
+            Console.WriteLine(conta.titular.cpf);
+        }
+    }
+}
