ec9ac1f129d2ce8280c94ddd68354a7f3c7cbc76
chrystian farias <chrstianb4mv@gmail.com>
2022-02-01 16:20:52 -0400

    .

---
+++ ByteBank_pt_2/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+        public double GetBonificacao()
+        {
+
+            Salario += Salario * 0.15;
+            return Salario;
+
+        }
+    }
+}
+
---
+++ ByteBank_pt_2/ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario bruno = new Funcionario();
+
+            bruno.CPF = "20098787";
+            bruno.nome = "Bruno Da Silva Pinhiro";
+            bruno.Salario = 3000;
+
+            Console.WriteLine(bruno.Salario);
+            Console.WriteLine(bruno.GetBonificacao());
+            Console.WriteLine(bruno.Salario);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
