e03dbbcef56ed84305bbe26b3c740b6b8f752337
chrystian farias <chrystianfarias@hotail.com>
2022-02-09 11:23:24 -0400

    add dados dos funcionarios

--- ByteBank_pt_2/ByteBank/Program.cs
+++ ByteBank_pt_2/ByteBank/Program.cs
@@ -13,26 +13,25 @@ namespace ByteBank
         static void Main(string[] args)
         {
             GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
-            Funcionario bruno = new Funcionario(300,"Bruno pereira","0989829");
-            Diretor andre = new Diretor("Andre Rocha", "3287487");
-            GerenteDeConta ana = new GerenteDeConta("dfdf","dwfds");
+
+            Diretor chrystian = new Diretor("Chrystian Pinheiro","09878787656");
+            Designer angela = new Designer("Angela Assis","98787654333");
+            GerenteDeConta carlos = new GerenteDeConta("Carlos Guina","87656789000");
+            Auxiliar marcela = new Auxiliar("Marcela Vitória","98787789876");
+
+
+            //prints
+
+            Console.WriteLine($"Diretor: {chrystian.Nome}. salario base: {chrystian.Salario}." +
+                $"bonificação:{chrystian.GetBonificacao()}.");
+
+            chrystian.ObterAumentoSalario();
+
+            Console.WriteLine("novo salario:" +chrystian.Salario);
             
            
            
 
-            Console.WriteLine(bruno.Nome);
-            Console.WriteLine(andre.Nome);
-            Console.WriteLine(Funcionario.TotalDeFuncionarios);
-            Console.WriteLine(bruno.Salario);
-            bruno.ObterAumentoSalario();
-            
-
-          
-
-            Console.WriteLine(andre.Salario);
-            andre.ObterAumentoSalario();
-            Console.WriteLine(andre.Salario);
-            Console.WriteLine(bruno.Salario);
             
 
 
